6e2c96c2c69d4f77111c94333f125577d943fa25
Wyatt Cuypers <102075671+wyatt-cuypers@users.noreply.github.com>
2022-08-17 08:10:35 -0500

    Add files via upload

---
+++ EmployeeAlertPopup/EmployeeAlert/UpcomingEventsForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlertFormV2
+{
+    public partial class UpcomingEventsForm : Form
+    {
+        static int clickCounter = 0;
+        public UpcomingEventsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UpcomingEventsForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+    }
+}
